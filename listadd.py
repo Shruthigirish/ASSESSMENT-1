9dcbe7805ad8d94f5f1796267c70135cd4255a24
Shruthigirish <79131620+Shruthigirish@users.noreply.github.com>
2023-02-19 15:33:51 +0530

    Update listadd.py

--- listadd.py
+++ listadd.py
@@ -1,8 +1,6 @@
 #This program takes the input of list from user and adds it
 def sum_numbers(list1):
     total = 0
-    # for number in numbers:
-    #      total += number
     for elements in range(0, len(list1)):
      total = total + list1[elements]
     return total
@@ -12,8 +10,5 @@ for i in range(0,num_elements):
     elements = int(input(0))
     list1.append(elements)
 print("The list entered by user is :", list1)
-# for elements in range(0, len(list1)):
-#     total = total + list1[elements]
-# print("Sum of all elements in given list: ", total)
 sum_numbers([list1])
 print("Sum of all elements in given list: ", sum_numbers)
